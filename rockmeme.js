45caa7c1725322de97cdf2d12bb0f73f860e37d5
Cooper <coop@coopeeo.xyz>
2025-09-15 16:33:22 -0500

    add audio to rock meme

--- rockmeme.js
+++ rockmeme.js
@@ -2,14 +2,16 @@ function rockmeme(image) {
   const style = document.createElement("style")
   style.textContent = "img.rockmeme {\nz-index:999999999;\nposition: absolute;\ntop:0;\nleft:0;\nheight:100%;\nwidth:100%;\nobject-fit: fill;\n}\n@keyframes rockmeme {\nfrom {\nopacity: 1;\n}\nto {\nopacity: 0;\n}\n}"
   document.head.appendChild(style)
+  const audio = Audio("https://www.myinstants.com/media/sounds/the-rock-sound-effect.mp3")
   const runIt = () => {
     const imageElement = document.createElement("img")
     imageElement.classList.add("rockmeme")
     imageElement.setAttribute("src", image)
     document.body.appendChild(imageElement)
+    audio.play()
     setTimeout(() => {
       imageElement.remove()
-    }, 1000);
+    }, 5500);
   }
   addEventListener("keydown", runIt)
   addEventListener("keyup", runIt)
