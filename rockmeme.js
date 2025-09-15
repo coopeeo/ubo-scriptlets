886edce134b40057c334e6fd212207275883a63f
Cooper <coop@coopeeo.xyz>
2025-09-15 16:20:50 -0500

    Fix style property assignment in rockmeme function

--- rockmeme.js
+++ rockmeme.js
@@ -1,6 +1,6 @@
 function rockmeme(image) {
   const style = document.createElement("style")
-  style.innerText = "img.rockmeme {\nz-index:999999999;\nposition absoulute;\ntop:0;\nleft:0;\nheight:100%;\nwidth:100%;\nobject-fit: fill;\n}\n@keyframes rockmeme {\nfrom {\nopacity: 1;\n}\nto {\nopacity: 0;\n}\n}"
+  style.textContent = "img.rockmeme {\nz-index:999999999;\nposition absoulute;\ntop:0;\nleft:0;\nheight:100%;\nwidth:100%;\nobject-fit: fill;\n}\n@keyframes rockmeme {\nfrom {\nopacity: 1;\n}\nto {\nopacity: 0;\n}\n}"
   document.head.appendChild(style)
   const runIt = () => {
     const imageElement = document.createElement("img")
