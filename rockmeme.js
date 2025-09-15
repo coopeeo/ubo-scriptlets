28629b6d65e21a7b53fb467b8f7f871a6d0f2ce1
Cooper <coop@coopeeo.xyz>
2025-09-15 16:56:14 -0500

    Update rockmeme.js

--- rockmeme.js
+++ rockmeme.js
@@ -1,3 +1,6 @@
+/// alias rm.js
+/// dependency run-at.fn
+/// world ISOLATED
 function rockmeme(image) {
   const style = document.createElement("style")
   style.textContent = "img.rockmeme {\nanimation: rockmeme 1s;\nopacity:0;\nz-index:999999999;\nposition: fixed;\ntop:0;\nleft:0;\nheight:100%;\nwidth:100%;\nobject-fit: fill;\n}\n@keyframes rockmeme {\n0% {\nopacity:0;\n}\n1% {\nopacity: 1;\n}\n100% {\nopacity: 0;\n}\n}"
