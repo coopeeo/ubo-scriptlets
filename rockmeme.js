37e6f602654630fbfc50b1c83e2a297d51a152ec
Cooper <coop@coopeeo.xyz>
2025-09-15 16:34:00 -0500

    oops

--- rockmeme.js
+++ rockmeme.js
@@ -2,7 +2,7 @@ function rockmeme(image) {
   const style = document.createElement("style")
   style.textContent = "img.rockmeme {\nz-index:999999999;\nposition: absolute;\ntop:0;\nleft:0;\nheight:100%;\nwidth:100%;\nobject-fit: fill;\n}\n@keyframes rockmeme {\nfrom {\nopacity: 1;\n}\nto {\nopacity: 0;\n}\n}"
   document.head.appendChild(style)
-  const audio = Audio("https://www.myinstants.com/media/sounds/the-rock-sound-effect.mp3")
+  const audio = new Audio("https://www.myinstants.com/media/sounds/the-rock-sound-effect.mp3")
   const runIt = () => {
     const imageElement = document.createElement("img")
     imageElement.classList.add("rockmeme")
