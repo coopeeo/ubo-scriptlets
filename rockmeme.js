ce7644f7ce50aa5d5fe279ffb9f48caf6f719985
Cooper <coop@coopeeo.xyz>
2025-09-15 16:44:09 -0500

    add opacity

--- rockmeme.js
+++ rockmeme.js
@@ -1,6 +1,6 @@
 function rockmeme(image) {
   const style = document.createElement("style")
-  style.textContent = "img.rockmeme {\nanimation: rockmeme 5s;\nz-index:999999999;\nposition: absolute;\ntop:0;\nleft:0;\nheight:100%;\nwidth:100%;\nobject-fit: fill;\n}\n@keyframes rockmeme {\n0% {\nopacity:0;\n}\n1% {\nopacity: 1;\n}\n100% {\nopacity: 0;\n}\n}"
+  style.textContent = "img.rockmeme {\nanimation: rockmeme 1s;\nopacity:0;\nz-index:999999999;\nposition: absolute;\ntop:0;\nleft:0;\nheight:100%;\nwidth:100%;\nobject-fit: fill;\n}\n@keyframes rockmeme {\n0% {\nopacity:0;\n}\n1% {\nopacity: 1;\n}\n100% {\nopacity: 0;\n}\n}"
   document.head.appendChild(style)
   const audio = new Audio("https://www.myinstants.com/media/sounds/the-rock-sound-effect.mp3")
   const runIt = () => {
@@ -12,7 +12,7 @@ function rockmeme(image) {
     audio.play()
     setTimeout(() => {
       imageElement.remove()
-    }, 5500);
+    }, 1200);
   }
   addEventListener("keydown", runIt)
   addEventListener("keyup", runIt)
